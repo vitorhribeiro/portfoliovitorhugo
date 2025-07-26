c72616e153bae180cb45720094fd49d35f36a66e
Vitor Hugo <vitorh.ctt@gmail.com>
2025-07-26 18:28:06 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// script.js vazio
+function toggleMenu() {
+    const menu = document.getElementById("menu-slide");
+    const overlay = document.getElementById("overlay");
+    menu.classList.toggle("open");
+    overlay.classList.toggle("show");
+  }
+
+  function toggleTextoFinneo() {
+    const curta = document.querySelector('.texto-finneo.curta');
+    const completa = document.querySelector('.texto-finneo.completa');
+    const botao = document.querySelector('.btn-vermais');
+
+    if (completa.classList.contains('hidden')) {
+      completa.classList.remove('hidden');
+      botao.textContent = 'Ver menos';
+    } else {
+      completa.classList.add('hidden');
+      botao.textContent = 'Ver mais';
+    }
+  }
